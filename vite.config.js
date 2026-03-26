fcc94dbb05d02186d5d3918a23281c3c1f9c34ad
Tushar Sibal <tushar.sibal@indmoney.com>
2026-03-26 20:29:23 +0530

    fix: base path updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dynamic-layout-demo/',
   plugins: [react()],
   server: {
     port: 7000,
